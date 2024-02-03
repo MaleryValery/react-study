6678c3f91f311d4aa2c66ac484c3300f145d71f2
Valeriia Melnikov <valery.medvedeva@gmail.com>
2024-02-03 12:25:09 +0200

    feat: update app component complite base functionality

--- use-popcorn/src/components/App.tsx
+++ use-popcorn/src/components/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Nav from './Nav';
 import Main from './Main';
 
@@ -10,73 +10,108 @@ import ListBox from './ListBox';
 import MovieList from './MovieList';
 import WatchedSummary from './WatchedSummary';
 import WatchedMoviesList from './WatchedMoviesList';
+import Loader from './Loader';
+import ErrorMessage from './ErrorMessage';
+import SelectedMovie from './SelectedMovie';
 
-const tempMovieData = [
-  {
-    imdbID: 'tt1375666',
-    Title: 'Inception',
-    Year: '2010',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-  },
-  {
-    imdbID: 'tt0133093',
-    Title: 'The Matrix',
-    Year: '1999',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg',
-  },
-  {
-    imdbID: 'tt6751668',
-    Title: 'Parasite',
-    Year: '2019',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg',
-  },
-];
-
-const tempWatchedData = [
-  {
-    imdbID: 'tt1375666',
-    Title: 'Inception',
-    Year: '2010',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg',
-    runtime: 148,
-    imdbRating: 8.8,
-    userRating: 10,
-  },
-  {
-    imdbID: 'tt0088763',
-    Title: 'Back to the Future',
-    Year: '1985',
-    Poster:
-      'https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg',
-    runtime: 116,
-    imdbRating: 8.5,
-    userRating: 9,
-  },
-];
+import { MovieType, SearchResponse, WatchedMovieType } from '../utils/types';
+import { API_KEY } from '../utils/consts';
 
 export default function App() {
-  const [movies, setMovies] = useState(tempMovieData);
-  const [watched, setWatched] = useState(tempWatchedData);
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState<MovieType[]>([]);
+  const [watched, setWatched] = useState<WatchedMovieType[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+  const [selectedId, setSelectedId] = useState('');
+
+  const handleCloseMovie = () => {
+    setSelectedId('');
+  };
+
+  const handleSelectMovie = (movieId: string) => {
+    setSelectedId(selectedId === movieId ? '' : movieId);
+  };
+
+  const handleWatchedMovie = (movie: WatchedMovieType) => {
+    setWatched((watchedList) => [...watchedList, movie]);
+  };
+
+  const handleDeleteWatched = (currentMovie: WatchedMovieType) => {
+    setWatched((watchedList) =>
+      watchedList.filter((movieEl) => movieEl.imdbID !== currentMovie.imdbID)
+    );
+  };
+
+  useEffect(() => {
+    const controller = new AbortController();
+    async function fetchMovie() {
+      if (query.length < 3) {
+        setMovies([]);
+        setError('');
+        return;
+      }
+      try {
+        setError('');
+        setIsLoading(true);
+
+        const res = await fetch(
+          `http://www.omdbapi.com/?apikey=${API_KEY}&s=${query}`,
+          { signal: controller.signal }
+        );
+        if (!res.ok) throw new Error('something went wrong =(');
+        const data: SearchResponse = await res.json();
+
+        if (data.Response === 'False') throw new Error('Nothing is found');
+        setMovies(() => data.Search);
+      } catch (e: unknown) {
+        if (e instanceof Error && e.name !== 'AbortError') {
+          setError(e.message);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchMovie();
+
+    return () => {
+      controller.abort();
+    };
+  }, [query]);
 
   return (
     <>
       <Nav>
         <Logo />
-        <Search />
+        <Search query={query} setQuery={setQuery} />
         <NumResults movieList={movies} />
       </Nav>
       <Main>
         <ListBox>
-          <MovieList movieList={movies} />
+          {isLoading && !error && <Loader />}
+          {!isLoading && !error && (
+            <MovieList onSelectMovie={handleSelectMovie} movieList={movies} />
+          )}
+          {error && <ErrorMessage message={error} />}
         </ListBox>
 
         <ListBox>
-          <WatchedSummary watched={watched} />
-          <WatchedMoviesList watched={watched} />
+          {selectedId ? (
+            <SelectedMovie
+              watchedList={watched}
+              selectedId={selectedId}
+              onCloseMovie={handleCloseMovie}
+              onAddWatchedMovie={handleWatchedMovie}
+            />
+          ) : (
+            <>
+              <WatchedSummary watched={watched} />
+              <WatchedMoviesList
+                onDeleteMovie={handleDeleteWatched}
+                watched={watched}
+              />
+            </>
+          )}
         </ListBox>
       </Main>
     </>
