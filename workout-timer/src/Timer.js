e678c4a0503b9cb11b153b8b7862443adf3987b0
Valeriia Melnikov <valeriia.melnikov@gmail.com>
2024-05-10 22:41:46 +0300

    feat: init and add memo

---
+++ workout-timer/src/Timer.js
@@ -0,0 +1,26 @@
+import { memo, useEffect, useState } from 'react';
+
+function Timer() {
+  const [time, setTime] = useState(formatTime(new Date()));
+  function formatTime(date) {
+    return new Intl.DateTimeFormat('en', {
+      month: 'short',
+      year: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+    }).format(date);
+  }
+
+  useEffect(function () {
+    const id = setInterval(function () {
+      setTime(formatTime(new Date()));
+    }, 1000);
+
+    return () => clearInterval(id);
+  }, []);
+
+  return <time>For your workout on {time}</time>;
+}
+
+export default memo(Timer);
