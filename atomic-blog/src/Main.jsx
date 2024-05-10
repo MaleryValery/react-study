e55bb0936b06485d020e56c770a5008db65ba9b6
Valeriia Melnikov <valeriia.melnikov@gmail.com>
2024-05-10 18:40:27 +0300

    feat: memoization practice

---
+++ atomic-blog/src/Main.jsx
@@ -0,0 +1,11 @@
+import { FormAddPost } from "./FormAddPost";
+import { Posts } from "./Post";
+
+export function Main() {
+  return (
+    <main>
+      <FormAddPost />
+      <Posts />
+    </main>
+  );
+}
